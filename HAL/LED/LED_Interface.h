65767ae061c9784ab30c4be2ad65369d685bb7f1
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-15 20:49:03 +0200

    Add files via upload

---
+++ HAL/LED/LED_Interface.h
@@ -0,0 +1,40 @@
+/***********************************************************************************************/
+/***********************************************************************************************/
+/************************************* Author: Fatema Ahmed ************************************/
+/***************************************** Layer: HAL ******************************************/
+/**************************************** Component: LED ***************************************/
+/***********************************************************************************************/
+/***********************************************************************************************/
+
+
+/*************************************************************************************************************************/
+/** Define The Port used in the Hardware  -->  PORTA or PORTB or PORTC or PORTD											**/
+/**																														**/
+/**	Define The Pin used in the Hardware   -->  PIN0 or PIN1 or PIN2 or PIN3 or PIN4 or PIN5 or PIN6 or PIN7			    **/
+/**																														**/
+/**	Define The Connection Type            -->  Source  or Sink								                            **/
+/**                                                                                                                     **/
+/**	1) Source Connection --> Connect the +ve of the led with the Microcontroller Pin and the -ve of the led to ground   **/
+/**																														**/
+/**	2) Sink Connection   --> Connect the -ve of the led with the Microcontroller Pin and the +ve of the led to 5 volt   **/
+/**																														**/
+/*************************************************************************************************************************/
+
+#ifndef LED_Interface
+#define LED_Interface
+
+typedef struct _LED_ LED;
+struct _LED_
+{
+	u8 PORT:2;
+	u8 PIN:3;
+	u8 Connection:1;
+};
+
+#define Source	1
+#define Sink	0
+
+void TurnLEDON(LED Led);
+void TurnLEDOFF(LED Led);
+
+#endif
--- HAL/LED/LED_Program.c
+++ HAL/LED/LED_Program.c
@@ -6,7 +6,28 @@
 /***********************************************************************************************/
 /***********************************************************************************************/
 
-#ifndef LED_Config
-#define LED_Config
+#include "STD_Types.h"
+#include "BitOperations.h"
 
-#endif
+#include "DIO_Interface.h"
+#include "LED_Config.h"
+#include "LED_Private.h"
+#include "LED_Interface.h"
+
+void TurnLEDON(LED Led)
+{
+	if (Led.Connection == Source)
+		DIO_SetPinValue (Led.PORT, Led.PIN, PIN_HIGH);
+		
+	else if (Led.Connection == Sink)
+		DIO_SetPinValue (Led.PORT, Led.PIN, PIN_LOW);
+}
+
+void TurnLEDOFF(LED Led)
+{
+	if (Led.Connection == Source)
+	DIO_SetPinValue (Led.PORT, Led.PIN, PIN_LOW);
+	
+	else if (Led.Connection == Sink)
+	DIO_SetPinValue (Led.PORT, Led.PIN, PIN_HIGH);
+}
