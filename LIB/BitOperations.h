e4e7a90d3ae1da1029f215f8b9bc743b832aeca2
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-15 21:05:15 +0200

    Add files via upload

--- LIB/BitOperations.h
+++ LIB/BitOperations.h
@@ -1,1 +1,9 @@
+#ifndef BITOPERATIONS_H_INCLUDED
+#define BITOPERATIONS_H_INCLUDED
 
+#define SET_BIT(x,BIT)    x|=(1<<BIT)
+#define CLR_BIT(x,BIT)    x&=(~(1<<BIT))
+#define TOGGLE_BIT(x,BIT) x^=(1<<BIT)
+#define GET_BIT(x,BIT)    (x>>BIT)&1
+
+#endif
