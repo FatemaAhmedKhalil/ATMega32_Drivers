56348b4133a15040e9f96d45a5a4eac9bbf54be4
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-15 20:57:10 +0200

    Add files via upload

---
+++ HAL/Seven Segment/Seg_Interface.h
@@ -0,0 +1,41 @@
+/***********************************************************************************************/
+/***********************************************************************************************/
+/************************************* Author: Fatema Ahmed ************************************/
+/***************************************** Layer: HAL ******************************************/
+/************************************** Component: 7Segment ************************************/
+/***********************************************************************************************/
+/***********************************************************************************************/
+
+
+/*****************************************************************************************************************************************/
+/** Define The Port used in the Hardware  -->  PORTA or PORTB or PORTC or PORTD															**/
+/**																																		**/
+/**	Define The COM Port and Pin used in the Hardware   -->  PIN0 or PIN1 or PIN2 or PIN3 or PIN4 or PIN5 or PIN6 or PIN7			    **/
+/**																																		**/
+/**	Define The Connection Type            -->  COMAnode  or COMCathod																	**/
+/**																																		**/
+/**	1) Common Cathod Connection  --> Connect the COM of the 7 Segment with the Ground	( Signal to Light the LEDs is HIGH )		    **/
+/**																																		**/
+/**	2) Common Anode Connection   --> Connect the COM of the 7 Segment with the 5 volt		( Signal to Light the LEDs is LOW )			**/
+/**																																		**/
+/*****************************************************************************************************************************************/
+
+
+#ifndef Seg_Interface
+#define Seg_Interface
+
+typedef struct _7Segment_ SEG;
+struct _7Segment_
+{
+	u8 PORT:2;
+	u8 COM_PORT:2;
+	u8 COM_PIN:3;
+	u8 COM_Connection:1;
+};
+
+#define COMAnode	1
+#define COMCathod	0
+
+void DisplaySevenSegment (SEG Segment, u8 Number);
+
+#endif
--- HAL/Seven Segment/Seg_Program.c
+++ HAL/Seven Segment/Seg_Program.c
@@ -5,10 +5,27 @@
 /************************************** Component: 7Segment ************************************/
 /***********************************************************************************************/
 /***********************************************************************************************/
+#include "STD_Types.h"
+#include "BitOperations.h"
 
-#ifndef Seg_Private
-#define Seg_Private
+#include "DIO_Interface.h"
+#include "Seg_Config.h"
+#include "Seg_Private.h"
+#include "Seg_Interface.h"
 
-#define SegmentNumbers	10
-
-#endif
+void DisplaySevenSegment (SEG Segment, u8 Number)
+{
+	static u8 Seg[SegmentNumbers] = Seg_Array;
+	
+	if (Segment.COM_Connection == COMAnode)
+	{
+		DIO_SetPinValue(Segment.COM_PORT, Segment.COM_PIN, PIN_HIGH); 
+		DIO_SetPortValue (Segment.PORT, ~Seg[Number]);	// Common Anode
+	}
+	
+	else if (Segment.COM_Connection == COMCathod)
+	{
+		DIO_SetPinValue(Segment.COM_PORT, Segment.COM_PIN, PIN_LOW); 
+		DIO_SetPortValue (Segment.PORT, Seg[Number]);	// Common Cathode
+	}
+}
