5240465916ff288dc6284168815b8b09b321b9c8
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-15 21:01:59 +0200

    Add files via upload

--- HAL/Switchs/Switch_Program.c
+++ HAL/Switchs/Switch_Program.c
@@ -6,30 +6,20 @@
 /***********************************************************************************************/
 /***********************************************************************************************/
 
+#include "STD_Types.h"
+#include "BitOperations.h"
 
-/**																															  **/
-/** Define The Switch Type		-->		PushButton or ToggleButton or DIPSwitch or LimitSwitch or ReedSwitch or RotarySwitch  **/
-/**																															  **/ 
+#include "DIO_Interface.h"
+#include "Switch_Config.h"
+#include "Switch_Private.h"
+#include "Switch_Interface.h"
 
-#ifndef Switch_Interface
-#define Switch_Interface
-
-typedef struct _Switch_ SWITCH;
-struct _Switch_
+u8 PressedButton(SWITCH Switch)
 {
-	u8 PORT:2;
-	u8 PIN:3;
-	u8 Type:3;
-};
-
-
-#define PushButton		0
-#define ToggleButton	1
-#define DIPSwitch		2
-#define LimitSwitch		3
-#define ReedSwitch		4
-#define RotarySwitch	5
-
-u8 PressedButton(SWITCH Switch);
-
-#endif
+	u8 SwitchState = 0;
+	
+	if (Switch.Type == PushButton)
+		DIO_GetPinValue (Switch.PORT, Switch.PIN, &SwitchState);
+	
+	return SwitchState;
+}
