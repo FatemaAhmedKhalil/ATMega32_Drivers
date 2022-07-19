b81745ea9d7505d1ee0144d0766b74d9644db001
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-20 00:14:14 +0200

    Add files via upload

--- MCAL/DIO/DIO_Register.h
+++ MCAL/DIO/DIO_Register.h
@@ -9,20 +9,20 @@
 #ifndef DIO_Register
 #define DIO_Register
 
-#define PORTA *((volatile u8*) 0X3b)	// Port A
-#define DDRA  *((volatile u8*) 0x3a)	// Port A Direction
-#define PINA  *((volatile u8*) 0x39)	// Pins of Port A
+#define PORTA *((volatile u8*) 0X3b)	// Port A Data Register
+#define DDRA  *((volatile u8*) 0x3a)	// Port A Data Direction Register
+#define PINA  *((volatile u8*) 0x39)	// Port A Input Pins
 
-#define PORTB *((volatile u8*) 0x38)	// Port B
-#define DDRB  *((volatile u8*) 0x37)	// Port B Direction
-#define PINB  *((volatile u8*) 0x36)	// Pins of Port B
+#define PORTB *((volatile u8*) 0x38)	// Port B Data Register
+#define DDRB  *((volatile u8*) 0x37)	// Port B Data Direction Register
+#define PINB  *((volatile u8*) 0x36)	// Port B Input Pins
 
-#define PORTC *((volatile u8*) 0x35)	// Port C
-#define DDRC  *((volatile u8*) 0x34)	// Port C Direction
-#define PINC  *((volatile u8*) 0x33)	// Pins of Port C
+#define PORTC *((volatile u8*) 0x35)	// Port C Data Register
+#define DDRC  *((volatile u8*) 0x34)	// Port C Data Direction Register
+#define PINC  *((volatile u8*) 0x33)	// Port C Input Pins
 
-#define PORTD *((volatile u8*) 0x32)	// Port D
-#define DDRD  *((volatile u8*) 0x31)	// Port D Direction
-#define PIND  *((volatile u8*) 0x30)	// Pins of Port D
+#define PORTD *((volatile u8*) 0x32)	// Port D Data Register
+#define DDRD  *((volatile u8*) 0x31)	// Port D Data Direction Register
+#define PIND  *((volatile u8*) 0x30)	// Port D Input Pins
 
 #endif
