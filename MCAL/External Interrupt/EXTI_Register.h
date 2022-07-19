bdf47915981ca8e8214f94a12b0331fb0d458977
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-20 00:34:28 +0200

    Add files via upload

--- MCAL/External Interrupt/EXTI_Register.h
+++ MCAL/External Interrupt/EXTI_Register.h
@@ -9,20 +9,21 @@
 #ifndef EXTI_Register
 #define EXTI_Register
 
-#define GICR	*((volatile u8*) 0x5B)
-#define GICR_INT1 7
-#define GICR_INT0 6
-#define GICR_INT2 5
+#define GICR	*((volatile u8*) 0x5B)		// General Interrupt Control Register
+#define GICR_INT1 7		// GICR External Interrupt Request 1 Enable
+#define GICR_INT0 6		// GICR External Interrupt Request 0 Enable
+#define GICR_INT2 5		// GICR External Interrupt Request 2 Enable
 
-#define GIFR	*((volatile u8*) 0x5A)
+#define GIFR	*((volatile u8*) 0x5A)		// General Interrupt Flag Register
 
-#define MCUCR	*((volatile u8*) 0x55)
-#define MCUCR_ISC11 3
-#define MCUCR_ISC10 2
-#define MCUCR_ISC01 1
-#define MCUCR_ISC00 0
+#define MCUCR	*((volatile u8*) 0x55)		// MCU Control Register
+#define MCUCR_ISC11 3		// Interrupt Sense Control 1
+#define MCUCR_ISC10 2		// Interrupt Sense Control 1
+#define MCUCR_ISC01 1		// Interrupt Sense Control 0
+#define MCUCR_ISC00 0		// Interrupt Sense Control 0
 
-#define MCUCSR	*((volatile u8*) 0x54)
-#define MCUCSR_ISC2 6
+#define MCUCSR	*((volatile u8*) 0x54)		// MCU Control and Status
+Register
+#define MCUCSR_ISC2 6		//  Interrupt Sense Control 2
 
 #endif
