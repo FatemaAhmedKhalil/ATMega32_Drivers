e8d70dda05358a80b881a04ab45d488fd11cb4da
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-20 00:17:46 +0200

    Add files via upload

--- MCAL/PORT/PORT_Register.h
+++ MCAL/PORT/PORT_Register.h
@@ -9,14 +9,14 @@
 #ifndef PORT_Register
 #define PORT_Register
 
-#define DDRA  *((volatile u8*) 0x3a)
-#define DDRB  *((volatile u8*) 0x37)
-#define DDRC  *((volatile u8*) 0x34)
-#define DDRD  *((volatile u8*) 0x31)
+#define DDRA  *((volatile u8*) 0x3a)	//Port A Data Direction Register
+#define DDRB  *((volatile u8*) 0x37)	//Port B Data Direction Register
+#define DDRC  *((volatile u8*) 0x34)	//Port C Data Direction Register
+#define DDRD  *((volatile u8*) 0x31)	//Port D Data Direction Register
 
-#define PORTA *((volatile u8*) 0X3b)
-#define PORTB *((volatile u8*) 0x38)
-#define PORTC *((volatile u8*) 0x35)
-#define PORTD *((volatile u8*) 0x32)
+#define PORTA *((volatile u8*) 0X3b)	//Port A Data Register
+#define PORTB *((volatile u8*) 0x38)	//Port B Data Register
+#define PORTC *((volatile u8*) 0x35)	//Port C Data Register
+#define PORTD *((volatile u8*) 0x32)	//Port D Data Register
 
 #endif
