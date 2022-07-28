54d0c37bfbee4134337a3a5d8bdece003c29ae33
Fatema Ahmed <100099549+FatemaAhmedKhalil@users.noreply.github.com>
2022-07-28 21:03:33 +0200

    Add files via upload

--- HAL/Seven Segment/Seg_Interface.h
+++ HAL/Seven Segment/Seg_Interface.h
@@ -7,19 +7,25 @@
 /***********************************************************************************************/
 
 
-/*************************************************************************************************************************************************/
-/** Define The Port used in the Hardware  -->  PORTA or PORTB or PORTC or PORTD									**/
-/**																		**/
-/**	Define The COM Port and Pin used in the Hardware   -->  PIN0 or PIN1 or PIN2 or PIN3 or PIN4 or PIN5 or PIN6 or PIN7		        **/
-/**																		**/
-/**	Define The Connection Type            -->  COMAnode  or COMCathod									**/
-/**																		**/
-/**	1) Common Cathod Connection  --> Connect the COM of the 7 Segment with the Ground	( Signal to Light the LEDs is HIGH )		**/
-/**																		**/
-/**	2) Common Anode Connection   --> Connect the COM of the 7 Segment with the 5 volt		( Signal to Light the LEDs is LOW )	**/
-/**																		**/
-/*************************************************************************************************************************************************/
+/*****************************************************************************************************************************************/
+/** Define The Port used in the Hardware  -->  PORTA or PORTB or PORTC or PORTD															**/
+/**																																		**/
+/**	Define The COM Port and Pin used in the Hardware   -->  PIN0 or PIN1 or PIN2 or PIN3 or PIN4 or PIN5 or PIN6 or PIN7			    **/
+/**																																		**/
+/**	Define The Connection Type            -->  COMAnode  or COMCathod																	**/
+/**																																		**/
+/**	1) Common Cathod Connection  --> Connect the COM of the 7 Segment with the Ground	( Signal to Light the LEDs is HIGH )		    **/
+/**																																		**/
+/**	2) Common Anode Connection   --> Connect the COM of the 7 Segment with the 5 volt		( Signal to Light the LEDs is LOW )			**/
+/**																																		**/
+/*****************************************************************************************************************************************/
 
+/************************************************************************/
+/* For Multiplexing 7 Segments:											*/
+/*		 Best Presence of Vision POV = 50Hz : 70Hz = 15ms : 20ms		*/
+/*					70Hz --> Ghosting									*/
+/*					50Hz --> Flickering									*/
+/************************************************************************/
 
 #ifndef Seg_Interface
 #define Seg_Interface
